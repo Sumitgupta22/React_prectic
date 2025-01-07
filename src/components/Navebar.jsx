f8a63ffc214598ec2d4514b06fc010fa73503f0f
sumitgupta22 <sumitgupta969139@gmail.com>
2025-01-08 00:15:51 +0530

    componet in components

--- src/components/Navebar.jsx
+++ src/components/Navebar.jsx
@@ -1,16 +1,12 @@
 import React from "react";
+import Navebar2 from "./Navebar2";
 
 const Navebar = () => {
   return (
     <>
       <nav className="flex bg-emerald-400 py-5 px-10 items-center justify-between">
         <h2 className="text-2xl">logo</h2>
-        <div className="flex gap-8 items-center">
-          <h4 className="text-xl">About</h4>
-          <h4 className="text-xl">Contect</h4>
-          <h4 className="text-xl">Home</h4>
-          <h4 className="text-xl">Contect</h4>
-        </div>
+        <Navebar2 />
       </nav>
     </>
   );
